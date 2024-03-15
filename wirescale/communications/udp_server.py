d91f60f516416e7369ed1d690abdb057ab9eac96
Fernando Guarini <fernandoenzo@gmail.com>
2024-03-15 20:01:26 +0100

    Optimize function `occupy_port_41641`

--- wirescale/communications/udp_server.py
+++ wirescale/communications/udp_server.py
@@ -2,6 +2,7 @@
 # encoding:utf-8
 
 
+import sys
 from socket import AF_INET, SOCK_DGRAM, socket
 from time import sleep
 
@@ -15,11 +16,13 @@ class UDPServer:
     def occupy_port_41641(cls):
         if cls.UDPDummy is not None:
             return
-        if TSManager.local_port() != 41641:
-            return
-        TSManager.stop()
-        cls.UDPDummy = socket(AF_INET, SOCK_DGRAM)
-        cls.UDPDummy.bind(('localhost', 41641))
+        if TSManager.local_port() == 41641:
+            TSManager.stop()
+        try:
+            cls.UDPDummy = socket(AF_INET, SOCK_DGRAM)
+            cls.UDPDummy.bind(('localhost', 41641))
+        except:
+            print("Couldn't occupy port 41641", file=sys.stderr)
         TSManager.start()
         while not TSManager.is_running():
             sleep(0.5)
