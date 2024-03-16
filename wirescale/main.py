2e83f168d1379490940a535c6927382af3fb1bbf
Fernando Guarini <fernandoenzo@gmail.com>
2024-03-17 00:38:35 +0100

    Add little wrapper that makes start/stop systemd calls

--- wirescale/main.py
+++ wirescale/main.py
@@ -16,14 +16,23 @@ sys.tracebacklimit = 0
 os.umask(0o177)  # chmod 600
 
 
+def check_root():
+    try:
+        os.setuid(0)
+    except PermissionError:
+        print("Error: Wirescale daemon must be managed by root's systemd", file=sys.stderr, flush=True)
+        sys.exit(1)
+
+
 def main():
     parse_args()
     if ARGS.DAEMON:
+        check_root()
         systemd_exec_pid = int(os.environ.get('SYSTEMD_EXEC_PID', default=-1))
-        if os.geteuid() != 0 or systemd_exec_pid is None or os.getpgid(systemd_exec_pid) != os.getpgid(os.getpid()):
-            print('Error: Wirescale daemon must be launched as root via systemd', file=sys.stderr, flush=True)
-            sys.exit(1)
         if ARGS.START:
+            if systemd_exec_pid == -1 or os.getpgid(systemd_exec_pid) != os.getpgid(os.getpid()):
+                systemd = subprocess.run(['systemctl', 'start', 'wirescaled.service'], text=True)
+                sys.exit(systemd.returncode)
             systemd_envvars = ('LISTEN_PID', 'LISTEN_FDS', 'LISTEN_FDNAMES')
             if next((True for e in systemd_envvars if e not in os.environ), False):
                 print('Error: Wirescale needs a UNIX socket supplied by systemd', file=sys.stderr, flush=True)
@@ -31,6 +40,9 @@ def main():
             create_thread(TCPServer.run_server)
             create_thread(UnixServer.run_server)
         elif ARGS.STOP:
+            if systemd_exec_pid == -1 or os.getpgid(systemd_exec_pid) != os.getpgid(os.getpid()):
+                systemd = subprocess.run(['systemctl', 'stop', 'wirescaled.service'], text=True)
+                sys.exit(systemd.returncode)
             UnixClient.stop()
     elif ARGS.UPGRADE:
         UnixClient.upgrade()
