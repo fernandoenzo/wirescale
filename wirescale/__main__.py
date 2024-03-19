62ea7791d5b998fd18ff0cd5d3555966c9df3c5b
Fernando Guarini <fernandoenzo@gmail.com>
2024-03-19 20:40:04 +0100

    Add `scripts` folder to PyInstaller bundle

--- wirescale/__main__.py
+++ wirescale/__main__.py
@@ -5,7 +5,9 @@
 import sys
 from pathlib import Path
 
-sys.path.insert(0, str(Path(__file__).parent.parent.resolve()))
+GLOBAL_PARENT = Path(__file__).parent.resolve()
+SCRIPT_PATH = GLOBAL_PARENT.joinpath('scripts')
+sys.path.insert(0, str(GLOBAL_PARENT.parent))
 
 if __name__ == '__main__':
     from wirescale.main import main
